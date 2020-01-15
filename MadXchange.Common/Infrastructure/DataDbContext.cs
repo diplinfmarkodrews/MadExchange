8235040ca29bc86c2c9c41667b8bc09fc6182ef9
diplinfmarkodrews <diplinfmarkodrews@gmx.de>
2020-01-15 06:34:46 +0100

    packages updated

---
+++ MadXchange.Common/Infrastructure/DataDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MadXchange.Common.Infrastructure
+{
+    public class DataDbContext : DbContext
+    {
+    }
+}
---
+++ MadXchange.Common/Infrastructure/EFRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MadXchange.Common.Types;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+namespace MadXchange.Common.Infrastructure
+{
+    
+    
+    public class EfRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    {
+
+        #region Fields
+
+        protected DataDbContext Context;
+
+        #endregion
+
+        public EfRepository(DataDbContext context)
+        {
+            Context = context;
+        }
+
+        #region Public Methods
+
+        public async Task<T> GetById(Guid id) => await Context.Set<T>().FindAsync(id);
+
+        public Task<T> FirstOrDefault(Expression<Func<T, bool>> predicate)
+            => Context.Set<T>().FirstOrDefaultAsync(predicate);
+
+        public async Task Add(T entity)
+        {
+            // await Context.AddAsync(entity);
+            await Context.Set<T>().AddAsync(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public Task Update(T entity)
+        {
+            // In case AsNoTracking is used
+            Context.Entry(entity).State = EntityState.Modified;
+            return Context.SaveChangesAsync();
+        }
+
+        public Task Remove(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+            return Context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await Context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetWhere(Expression<Func<T, bool>> predicate)
+        {
+            return await Context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public Task<int> CountAll() => Context.Set<T>().CountAsync();
+
+        public Task<int> CountWhere(Expression<Func<T, bool>> predicate)
+            => Context.Set<T>().CountAsync(predicate);
+
+        #endregion
+
+    }
+    
+}
